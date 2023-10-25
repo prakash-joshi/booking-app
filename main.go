63b40d9b852c4041dd375a4160f5257b96c94e12
Prakash Joshi <prakash.joshi167@hotmail.com>
2023-10-25 17:45:35 +0530

    learned string and int data types syntax

--- main.go
+++ main.go
@@ -3,12 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Golang Conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	var conferenceName string = "Golang Conference"
+	const conferenceTickets int = 50
+	var remainingTickets int = 50
 
 	fmt.Printf("Welcome to %v Booking Application! \n", conferenceName)
 	fmt.Printf("Only %v tickets are still available from %v tickets. \n", remainingTickets, conferenceTickets)
 	fmt.Println("Get your tickets here to attend.")
+
+	var userName string
+	var userTicket int
+
+	userName = "Prakash"
+	userTicket = 23
+	fmt.Printf("User %v has ticket no : %v. \n", userName, userTicket)
+
+	fmt.Println("")
+
+	fmt.Printf("conferenceName is %T. \n", conferenceName)
+	fmt.Printf("conferenceTickets is %T. \n", conferenceTickets)
+	fmt.Printf("remainingTickets is %T. \n", remainingTickets)
+	fmt.Printf("userName is %T. \n", userName)
+	fmt.Printf("userTicket is %T. \n", userTicket)
 
 }
