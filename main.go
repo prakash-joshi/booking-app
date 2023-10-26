582f6e173d39493150b4ced393d25dec8dd57bc4
Prakash Joshi <prakash.joshi167@hotmail.com>
2023-10-26 22:54:46 +0530

    learned to create an array of type string

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ func main() {
 	var conferenceName string = "Golang Conference"
 	const conferenceTickets int = 50
 	var remainingTickets uint = 50
+	var bookings [50]string
 
 	fmt.Printf("Welcome to %v Booking Application! \n", conferenceName)
 	fmt.Printf("Only %v tickets are still available from %v tickets. \n", remainingTickets, conferenceTickets)
@@ -29,9 +30,14 @@ func main() {
 	fmt.Scan(&userTicket)
 
 	remainingTickets = remainingTickets - userTicket
+	bookings[0] = firstName + " " + lastName
+
+	fmt.Printf("The whole booking Array : %v.\n", bookings)
+	fmt.Printf("The booking Array first value : %v.\n", bookings[0])
+	fmt.Printf("The whole booking Array Type : %T.\n", bookings)
+	fmt.Printf("The whole booking Array length : %v.\n", len(bookings))
 
 	fmt.Printf("Thank You %v %v for booking %v tickts. You will receive the confirmation email at %v.\n", firstName, lastName, userTicket, email)
-
 	fmt.Printf("Only %v tickets remaining for %v.\n", remainingTickets, conferenceName)
 	// fmt.Println("")
 	// fmt.Printf("conferenceName is %T. \n", conferenceName)
