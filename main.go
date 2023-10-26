d17f461db5913cc073675163bd2ab8796306b6ac
Prakash Joshi <prakash.joshi167@hotmail.com>
2023-10-26 23:09:44 +0530

    learned slice which is the dynamic size list/array

--- main.go
+++ main.go
@@ -6,7 +6,8 @@ func main() {
 	var conferenceName string = "Golang Conference"
 	const conferenceTickets int = 50
 	var remainingTickets uint = 50
-	var bookings [50]string
+	// var bookings [50]string  ///Array with length 50
+	var bookings []string ///slice is an list an abstarct of array which has dynamic length
 
 	fmt.Printf("Welcome to %v Booking Application! \n", conferenceName)
 	fmt.Printf("Only %v tickets are still available from %v tickets. \n", remainingTickets, conferenceTickets)
@@ -30,12 +31,13 @@ func main() {
 	fmt.Scan(&userTicket)
 
 	remainingTickets = remainingTickets - userTicket
-	bookings[0] = firstName + " " + lastName
+	// bookings[0] = firstName + " " + lastName  ///used to acccess index of an array
+	bookings = append(bookings, firstName+" "+lastName) ///similar too push for array in js
 
-	fmt.Printf("The whole booking Array : %v.\n", bookings)
-	fmt.Printf("The booking Array first value : %v.\n", bookings[0])
-	fmt.Printf("The whole booking Array Type : %T.\n", bookings)
-	fmt.Printf("The whole booking Array length : %v.\n", len(bookings))
+	fmt.Printf("The whole booking Slice : %v.\n", bookings)
+	fmt.Printf("The booking Slice first value : %v.\n", bookings[0])
+	fmt.Printf("The whole booking Slice Type : %T.\n", bookings)
+	fmt.Printf("The whole booking Slice length : %v.\n", len(bookings))
 
 	fmt.Printf("Thank You %v %v for booking %v tickts. You will receive the confirmation email at %v.\n", firstName, lastName, userTicket, email)
 	fmt.Printf("Only %v tickets remaining for %v.\n", remainingTickets, conferenceName)
