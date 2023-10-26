bf9378fd563a6a0eedec9bde63b1072a88566f92
Prakash Joshi <prakash.joshi167@hotmail.com>
2023-10-26 21:48:51 +0530

    learned pointers and how to read values provided by user in cli

--- main.go
+++ main.go
@@ -11,19 +11,29 @@ func main() {
 	fmt.Printf("Only %v tickets are still available from %v tickets. \n", remainingTickets, conferenceTickets)
 	fmt.Println("Get your tickets here to attend.")
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTicket int
+	fmt.Print("Enter Your Fisrt Name : ")
+	fmt.Scan(&firstName)
 
-	userName = "Prakash"
-	userTicket = 23
-	fmt.Printf("User %v has ticket no : %v. \n", userName, userTicket)
+	fmt.Print("Enter Your Last Name : ")
+	fmt.Scan(&lastName)
 
-	fmt.Println("")
+	fmt.Print("Enter Your Email : ")
+	fmt.Scan(&email)
 
-	fmt.Printf("conferenceName is %T. \n", conferenceName)
-	fmt.Printf("conferenceTickets is %T. \n", conferenceTickets)
-	fmt.Printf("remainingTickets is %T. \n", remainingTickets)
-	fmt.Printf("userName is %T. \n", userName)
-	fmt.Printf("userTicket is %T. \n", userTicket)
+	fmt.Print("Enter No of Tickets : ")
+	fmt.Scan(&userTicket)
+
+	fmt.Printf("Thank You %v %v for booking %v tickts. You will receive the confirmation email at %v.\n", firstName, lastName, userTicket, email)
+
+	// fmt.Println("")
+	// fmt.Printf("conferenceName is %T. \n", conferenceName)
+	// fmt.Printf("conferenceTickets is %T. \n", conferenceTickets)
+	// fmt.Printf("remainingTickets is %T. \n", remainingTickets)
+	// fmt.Printf("userName is %T. \n", userName)
+	// fmt.Printf("userTicket is %v. \n", &userTicket)
 
 }
