9c5078968e2b047ae4124d378ff5e83dde87904c
Prakash Joshi <prakash.joshi167@hotmail.com>
2023-10-27 19:32:16 +0530

    learned if else conditions and used it for booking tickets

--- main.go
+++ main.go
@@ -9,14 +9,12 @@ func main() {
 	var conferenceName string = "Golang Conference"
 	const conferenceTickets int = 50
 	var remainingTickets uint = 50
-	// var bookings [50]string  ///Array with length 50
-	var bookings []string ///slice is an list an abstarct of array which has dynamic length
+	var bookings []string
 
 	fmt.Printf("Welcome to %v Booking Application! \n", conferenceName)
 	fmt.Printf("Only %v tickets are still available from %v tickets. \n", remainingTickets, conferenceTickets)
 	fmt.Println("Get your tickets here to attend.")
 	for {
-
 		var firstName string
 		var lastName string
 		var email string
@@ -24,37 +22,43 @@ func main() {
 
 		fmt.Print("Enter Your First Name : ")
 		fmt.Scan(&firstName)
-
 		fmt.Print("Enter Your Last Name : ")
 		fmt.Scan(&lastName)
-
 		fmt.Print("Enter Your Email : ")
 		fmt.Scan(&email)
-
 		fmt.Print("Enter No of Tickets : ")
 		fmt.Scan(&userTicket)
 
-		remainingTickets = remainingTickets - userTicket
-		// bookings[0] = firstName + " " + lastName  ///used to acccess index of an array
-		bookings = append(bookings, firstName+" "+lastName) ///similar too push for array in js
+		if userTicket <= remainingTickets {
+			remainingTickets = remainingTickets - userTicket
+			bookings = append(bookings, firstName+" "+lastName) ///similar too push for array in js
 
-		fmt.Printf("The whole booking Slice : %v.\n", bookings)
-		fmt.Printf("The booking Slice first value : %v.\n", bookings[0])
-		fmt.Printf("The whole booking Slice Type : %T.\n", bookings)
-		fmt.Printf("The whole booking Slice length : %v.\n", len(bookings))
+			fmt.Printf("Thank You %v %v for booking %v tickts. You will receive the confirmation email at %v.\n", firstName, lastName, userTicket, email)
+			fmt.Printf("Only %v tickets remaining for %v.\n", remainingTickets, conferenceName)
 
-		fmt.Printf("Thank You %v %v for booking %v tickts. You will receive the confirmation email at %v.\n", firstName, lastName, userTicket, email)
-		fmt.Printf("Only %v tickets remaining for %v.\n", remainingTickets, conferenceName)
+			firstNames := []string{}
 
-		firstNames := []string{}
+			for _, bookingElement := range bookings {
+				var name = strings.Fields(bookingElement)
+				firstNames = append(firstNames, name[0])
+			}
 
-		for _, bookingElement := range bookings {
-			var name = strings.Fields(bookingElement)
-			firstNames = append(firstNames, name[0])
+			fmt.Printf("These are the Names of our bookings : %v. \n", firstNames)
+
+			if remainingTickets == 0 {
+				fmt.Println("Sorry the confrence is sold out. Come back next Year.")
+				break
+			}
+		} else {
+			fmt.Printf("We only have %v tickets reamining. You cannot book %v tickets. Please Try again. \n", remainingTickets, userTicket)
+			continue
 		}
-		fmt.Printf("These are the Names of our bookings : %v. \n", firstNames)
-
 	}
+
+	// fmt.Printf("The whole booking Slice : %v.\n", bookings)
+	// fmt.Printf("The booking Slice first value : %v.\n", bookings[0])
+	// fmt.Printf("The whole booking Slice Type : %T.\n", bookings)
+	// fmt.Printf("The whole booking Slice length : %v.\n", len(bookings))
 	// fmt.Println("")
 	// fmt.Printf("conferenceName is %T. \n", conferenceName)
 	// fmt.Printf("conferenceTickets is %T. \n", conferenceTickets)
